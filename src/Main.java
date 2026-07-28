2f36faf3b94629279dc74157c3eb1b02b9933e65
mona <monatazarine@gmail.com>
2026-07-28 16:52:06 +0100

    this is my first java program

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args){
+
+        // this is my first java program
+        System.out.println("i like pizza");
+        System.out.println("it's really good!");
+        System.out.println("Buy me one !!!");
+    }
+}
