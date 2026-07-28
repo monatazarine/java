21c9cb011ddfd3b904ad0ff8704bf9ef2c2043ad
mona <monatazarine@gmail.com>
2026-07-28 18:46:30 +0100

    user input

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
 public class Main {
     public static void main (String[] args){
+        //user input
 
-        //variables
-        int age = 20 ;
-        boolean isOnline = true ;
-        String email = "fake123@gmail.com";
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("my user's age is " + age );
-        System.out.println("my user is online : " + isOnline);
-        System.out.println("my user's email is : " + email);
-
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("hello " + name);
+        System.out.println("Are you a student ?(true/false): ");
+        boolean isStudent = scanner.nextBoolean();
+        if (isStudent) {
+            System.out.println("You are enrolled as a student");
+        }
+        else {
+            System.out.println("you are NOT enrolled ");
+        }
+        scanner.close();
 
     }
 }
