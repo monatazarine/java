fefad7a27165890d5bf6ceb4d55fae2d04e0202a
mona <monatazarine@gmail.com>
2026-09-07 19:28:40 +0100

    Random

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,18 @@
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main (String[] args){
 
-        Scanner scanner = new Scanner(System.in);
-
-
-        String[] foods =   new String[3];
-        for (int i = 0; i < foods.length ;i++){
-            System.out.print("Enter the food : ");
-            foods[i] = scanner.nextLine();
-
-        }
-        for (String food : foods){
-            System.out.println(food);
+        Random random = new Random();
+        boolean isHeads;
+        isHeads = random.nextBoolean();
+        if(isHeads) {
+            System.out.println("Heads");
+        }else {
+            System.out.println("Tails");
         }
 
-        scanner.close();
+
 
 
 
