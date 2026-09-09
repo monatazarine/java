4acce6699839d008dbc63a3aee322601dc96916a
mona <monatazarine@gmail.com>
2026-09-09 14:09:58 +0100

    enter user input into an array

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,27 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String[] foods ;
+        int size ;
+        System.out.println("How many foods are you ordering ?: ");
+        size = scanner.nextInt();
+        scanner.nextLine();
+        foods = new String[size];
 
-        Random random = new Random();
-        boolean isHeads;
-        isHeads = random.nextBoolean();
-        if(isHeads) {
-            System.out.println("Heads");
-        }else {
-            System.out.println("Tails");
+        for (int i = 0; i < foods.length; i++) {
+            System.out.println("Enter a food : ");
+            foods[i] = scanner.nextLine();
         }
 
-
-
-
+        for (String food : foods ){
+            System.out.println(food);
+        }
+        scanner.close();
 
 
     }
+
+
 }
