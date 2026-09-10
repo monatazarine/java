3242780c9bcab25b9acfe5aabfa054e9d3af9e9a
mona <monatazarine@gmail.com>
2026-09-10 22:15:45 +0100

    SEARCH AN ARRAY

--- src/Main.java
+++ src/Main.java
@@ -1,113 +1,41 @@
-import java.util.Random;
+// SEARCH AN ARRAY
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main (String[] args){
-        //JAVA DICE ROLLER PROGRAM
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
 
-        // DECLARE VARS
-        int numOfDice ;
-        int total = 0;
+    Scanner scanner = new Scanner(System.in);
+    String[] fruits ={"Apple",
+            "Banana",
+            "Orange",
+            "Strawberry",
+            "Mango",
+            "Pineapple",
+            "Blueberry",
+            "Watermelon" };
+    boolean isFound = false;
+    String target ;
 
-        // GET NUMBER OF DICE FROM USER
-        System.out.print("Enter the number of dice to roll: ");
-        numOfDice = scanner.nextInt();
 
-
-
-        //CHECK IF NUMBER OF DICE > 0
-        if (numOfDice >  0 ){
-            //ROLL THE DICE
-            // GET TOTAL
-            for (int i = 0; i < numOfDice; i++) {
-                int roll = random.nextInt(1,7);
-                printDice(roll);
-                System.out.println("you rolled  : "+ roll);
-                total += roll ;
-
+    System.out.print("Search : ");
+    target = scanner.nextLine();
+    // Linear search
+        for (int i = 0; i < fruits.length; i++) {
+            if(fruits[i].equals(target)) {
+                System.out.println("Element at index : " + i);
+                isFound = true;
+                break;
             }
-            System.out.println("Total : "+ total);
-
-        }else {
-            System.out.println("Number of dice must be greater than 0 ");
         }
-
-
-
-
-
-
+        if(!isFound){
+            System.out.println("Element not found !!");
+        }
         scanner.close();
-
-
-
-
-
-
     }
-    // DISPLAY ASCII OF DICE
-    static void printDice(int roll){
 
-        String dice1 = """
-                 -------
-                |       |
-                |   ●   |
-                |       |
-                 -------
-                """;
-
-        String dice2 = """
-                 -------
-                |     ● |
-                |       |
-                | ●     |
-                 -------
-                """;
-
-        String dice3 = """
-                 -------
-                |     ● |
-                |   ●   |
-                | ●     |
-                 -------
-                """;
-        String dice4 = """
-                 -------
-                | ●   ● |
-                |       |
-                | ●   ● |
-                 -------
-                """;
-        String dice5 = """
-                 -------
-                | ●   ● |
-                |   ●   |
-                | ●   ● |
-                 -------
-                """;
-        String dice6 = """
-                 -------
-                | ●   ● |
-                | ●   ● |
-                | ●   ● |
-                 -------
-                """;
-
-        switch (roll){
-            case 1 -> System.out.print(dice1);
-            case 2 -> System.out.print(dice2);
-            case 3 -> System.out.print(dice3);
-            case 4 -> System.out.print(dice4);
-            case 5 -> System.out.print(dice5);
-            case 6 -> System.out.print(dice6);
-            default -> System.out.println("INVALID ROLL");
-
-
-        }
     }
 
 
-}
+
