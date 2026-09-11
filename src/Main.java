99555ccd2cec6fe0daf9362df640d817e8977970
mona <monatazarine@gmail.com>
2026-09-12 00:20:29 +0100

    2D arrays 2

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,49 @@
-
+//2D arrays = an array where each el is an array useful for storing a matrix of data
 public class Main {
 
     public static void main (String[] args){
-        System.out.println(add(1 ,2 ,3 ,4));
-
-
-    }
-
-    static int add(int...numbers){
-        int sum = 0 ;
-
-        for (int number : numbers) {
-            sum += number ;
-
+        String[] fruits = {
+                "Apple",
+                "Banana",
+                "Orange",
+                "Strawberry",
+                "Mango",
+                "Pineapple",
+                "Blueberry",
+                "Watermelon"
+        };
+        String[] vegetables = {
+                "Carrot",
+                "Broccoli",
+                "Spinach",
+                "Potato",
+                "Tomato",
+                "Cucumber",
+                "Bell Pepper",
+                "Onion"
+        };
+        String[] meats = {
+                "Chicken",
+                "Beef",
+                "Pork",
+                "Lamb",
+                "Turkey",
+                "Duck",
+                "Veal",
+                "Venison"
+        };
+        String[][] groceries = {{"Apple","Banana","Orange"},
+                                {"Carrot","Broccoli","Potato"},
+                                {"Chicken","Beef","Fish"}};
+        groceries[0][0] = "Kiwi";
+        for (String[] foods: groceries){
+            for(String food: foods){
+                System.out.println(food );
+            }
         }
-        return sum;
-
     }
 
-    }
+}
 
 
 
