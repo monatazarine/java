1bf2a48a17f2a0cda8465134f4f12f4f287c1734
mona <monatazarine@gmail.com>
2026-09-24 20:18:27 +0000

    how to write a file

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,31 @@
-
-//Exception = An event that interrupts the normal flow of a program
-
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        //how to write a file
+        //fileWrite = for small & medium-sized text files
+        //BufferedWrite = better performance for large amounts of text
+        //PrintWrite = for structured data , like reports or logs
+        //FileOutputStream = for binary files (images, audio files ...)
 
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Enter a number :");
-            int number = scanner.nextInt();
-            System.out.println(number);
-        } catch (ArithmeticException e) {
-            System.out.println("YOU CAN'T DIVIDE BY ZERO !");
-        } catch (InputMismatchException e) {
-            System.out.println("YOU NEED TO ENTER A NUMBER !");
-        } catch (Exception e) {
-            System.out.println("Something went wrong");
-        } finally {
-            System.out.println("This will always be executed");
+        String path = "C:\\Users\\PC MGOUNA\\Desktop\\java\\POO\\test.txt";
+        String txtContent  = """
+                I like coffee ☕!!!!
+                I drink a cup every morning !
+                It gives me the energy needed for the day.
+                """;
+        try(FileWriter writer = new FileWriter(path)) {
+            writer.write(txtContent);
+            System.out.println("File has been written ");
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not locate file location");
         }
 
+        catch (IOException e) {
+            System.out.println("Could not write file !");
+        }
 
     }
 }
