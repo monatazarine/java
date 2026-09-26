9e8eb1ed7dd756728234d6bb1f0f61dc194a6bc6
mona <monatazarine@gmail.com>
2026-09-26 01:47:04 +0000

    Hangman game

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,121 @@
-//Music player
-
-import javax.sound.sampled.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "src\\KarmaPolice.wavP";
-        File file = new File(filePath);
-         //AudioSystem.getAudioInputStream(file): Converts the file into a readable audio stream.
-        try (Scanner scanner = new Scanner(System.in) ;AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)){
+        String word = "pizza" ;
+
+        Scanner scanner = new Scanner(System.in);
+         ArrayList<Character> wordState = new ArrayList<>();  
+         int wrongGuesses = 0;
+         for(int i = 0; i < word.length(); i++) {
+             wordState.add('_');
+         }
+         
+         System.out.println("Welcome to Hangman!");
+         System.out.println("___________________");
 
 
-            //clip is like a music player that allows to load an audio file and then play ,pause or reset it
-            Clip clip = AudioSystem.getClip();
-            //to open the audio stream obj
-            clip.open(audioStream); //Loads the raw audio stream into memory so it's ready to play instantly.
 
-            //to play the audio
-            //clip.start();
+         while (wrongGuesses < 6 ) {
+             System.out.println(getHangmanART((wrongGuesses)));
+             System.out.print("Word: ");
+             for (char c : wordState) {
+                 System.out.print(c + " ");
+             }
+             System.out.println();
+             System.out.print("Guess a letter: ");
+             char guess = scanner.nextLine().toLowerCase().charAt(0);
+             
+             if (word.indexOf(guess) >= 0) {
+                 System.out.println("Correct !!");
+                 for (int i = 0; i < word.length(); i++) {
+                     if (word.charAt(i) == guess) {
+                         wordState.set(i, guess);
+                     }
+                 }
+                 if(!wordState.contains('_')){
+                     System.out.println(getHangmanART((wrongGuesses)));
+                     System.out.println("YOU WIN!\nYou've guessed the word: " + word);
+              
+                     break;
+                
+                 }
+             } else {
+                 wrongGuesses++;         
+                 System.out.println("Wrong guess! You have " + (6 - wrongGuesses) + " guesses left.");
+             }
+          
+         }    
+   if (wrongGuesses >= 6){
+                 System.out.println(getHangmanART(wrongGuesses));
+                 System.out.println("GAME OVER !");
+                 System.out.println("The word was : " + word);
+             }
+         scanner.close();
+    }
 
-            String response= "";
-            while (!response.equals("Q")){
-                System.out.println("__Menu__");
-                System.out.println("P = Play");
-                System.out.println("S = Stop");
-                System.out.println("Q = Quit");
-                System.out.println("_________");
-                System.out.print("Enter your Choice : ");
-
-                response = scanner.nextLine().toUpperCase();
-                switch ( response){
-                    case "P" -> clip.start();
-                    case "S" -> clip.stop();
-                    case "Q" -> clip.close();
-                    default -> System.out.println("INVALID CHOICE !!");
 
 
-                }
-
-            }
-
-
-        }catch (FileNotFoundException e ){
-            System.out.println("Unable to find file location !!");
-        }
-        catch (UnsupportedAudioFileException e){
-            System.out.println("Audio file not supported !");
-        } catch (LineUnavailableException e) {
-            System.out.println("Unable to access audio resource");
-        }
-        catch (IOException e) {
-            System.out.println("Something went wrong !!");
-        }
-        finally {
-            System.out.println("Done!");
-
-        }
+    static String getHangmanART(int wrongGuesses) {
+        return switch (wrongGuesses) {
+            case 0 -> """
+                    +---+
+                    |   |
+                        |
+                        |
+                        |
+                        |
+                    =========""";
+            case 1 -> """
+                    +---+
+                    |   |
+                    O   |
+                        |
+                        |
+                        |
+                    =========""";
+            case 2 -> """
+                    +---+
+                    |   |
+                    O   |
+                    |   |
+                        |
+                        |
+                    =========""";
+            case 3 -> """
+                     +---+
+                     |   |
+                     O   |
+                    /|   |
+                         |
+                         |
+                     =========""";
+            case 4 -> """
+                     +---+
+                     |   |
+                     O   |
+                    /|\\  |
+                         |
+                         |
+                     =========""";
+            case 5 -> """
+                     +---+
+                     |   |
+                     O   |
+                    /|\\  |
+                    /    |
+                         |
+                     =========""";
+            case 6 -> """
+                     +---+
+                     |   |
+                     O   |
+                    /|\\  |
+                    / \\  |
+                         |
+                     =========""";
+            default -> "";
+        };
     }
 }
